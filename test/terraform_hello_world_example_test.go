3409e59cb5388b7a844ee48b3b25acd5e819062b
Kory Jackson <kory.a.jaxn@gmail.com>
2022-01-21 10:23:27 -0800

    test requirements are added

--- test/terraform_hello_world_example_test.go
+++ test/terraform_hello_world_example_test.go
@@ -10,7 +10,7 @@ import (
 func TestTerraformHelloWorldExample(t *testing.T) {
 	// Construct the terraform options with default retryable errors to handle the most common
 	// retryable errors in terraform testing.
-	terraformOptions := &terraform.Options{
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
 		// Set the path to the Terraform code that will be tested.
 		TerraformDir: "../",
 	})
